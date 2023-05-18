a401c29fffe3e073dbc0aaea554f84376f888209
Daniel Dystrych <daniel.dystrych@gmail.com>
2023-05-18 22:37:11 +0200

    fix: repair port forwarding in docker file also repaird bug in main.py + small code formating

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 FROM python:3.9
 
-WORKDIR /app
+EXPOSE 8000 
+WORKDIR /code
 
-COPY requirements.txt requirements.txt
-RUN pip3 install -r requirements.txt
+COPY ./requirements.txt /code/requirements.txt
 
-COPY . . 
+RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt 
 
-
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
+copy ./app /code/app
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
--- app/main.py
+++ app/main.py
@@ -1,35 +1,32 @@
-import numpy, io
+import json
 from fastapi import FastAPI, UploadFile, File
 from torchvision.models import ResNet50_Weights,resnet50
 from torchvision.transforms import transforms
 from PIL import Image
-import json
 
+#initialize app
 app = FastAPI()
 
 
-# Step 1: Initialize model with the best available weights
+#intialize resnet and connected variables
 weights = ResNet50_Weights.DEFAULT
 model = resnet50(weights=weights)
 model.eval()
 transform = transforms.ToTensor()
-# Step 2: Initialize the inference transforms
+
 preprocess = weights.transforms()
 
-# Load class names
-with open('imagenet-simple-labels.json') as f:
-    class_names = json.load(f)
-
+#create endpoint
 @app.post("/predict/")
 async def predict(file: UploadFile = File(...)):
     # Load and preprocess image
     image = Image.open(file.file)
     image_tensor = transform(image)
 
-    # Step 3: Apply inference preprocessing transforms
+    # Apply preprocess
     batch = preprocess(image_tensor).unsqueeze(0)
 
-    # Step 4: Use the model and print the predicted category
+    # Use the model and return predicted category
     prediction = model(batch).squeeze(0).softmax(0)
     class_id = prediction.argmax().item()
     score = prediction[class_id].item()
